c7bbcbf00add1ce62038c7038b464e78f75de59e
Mehmet KARAKÖSE <mhtkarakose@gmail.com>
2018-08-01 05:11:24 +0300

    Header & Header-Alert
    Created Header & Header-Alert component

---
+++ src/app/header-alert/header-alert.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header-alert',
+  templateUrl: './header-alert.component.html',
+  styleUrls: ['./header-alert.component.css']
+})
+export class HeaderAlertComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
