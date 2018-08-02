70bc5f78ac8ee8152bea0bbb324865eb0a31ae6e
Mehmet KARAKÖSE <mhtkarakose@gmail.com>
2018-08-03 01:19:01 +0300

    created some components
    created footer homepage component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HeaderAlertComponent } from './header-alert/header-alert.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { ProductlistComponent } from './productlist/productlist.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HeaderAlertComponent
+    HeaderAlertComponent,
+    HomepageComponent,
+    ProductlistComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
